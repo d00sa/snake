64c378cbd5cbd206ab9500e4652ea5b7d89c0bc4
uyk0329 <69350384+uyk0329@users.noreply.github.com>
2024-06-15 03:21:36 +0900

    6.15
    Create:
    - Item

---
+++ Item.cpp
@@ -0,0 +1,55 @@
+#include "Item.h"
+#include <bits/stdc++.h>
+#define LoopCount 200
+using namespace std;
+
+Item::Item(Map _map, Snake _snake) {
+
+    for (int i = 0; i < LoopCount; i++) {
+        setItemPos(_map);
+        if (overlapWithSnake(_snake)) continue;
+        else break;
+    }
+
+}
+
+void Item::setItemPos(Map _map) {
+
+    srand((unsigned int)time(0));
+
+    int x = (rand() % (_map.GetMapCols - 1)) + 1;
+    int y = (rand() % (_map.GetMapRows - 1)) + 1;
+
+    if (_map._map[y][x] == 0) {
+        itemPos = pair <ll, ll> (y, x);
+        itemTypeCode = (rand() % 2 == 0 ? 1 : 0);
+    }
+    else {
+        while (true) {
+            if (_map._map[y][x] == 0) break;
+            int x = (rand() % (_map.GetMapCols - 1)) + 1;
+            int y = (rand() % (_map.GetMapRows - 1)) + 1;
+            if (_map._map[y][x] == 0) {
+                itemPos = pair <ll, ll> (y, x);
+                itemTypeCode = (rand() % 2 == 0 ? 1 : 0);
+            }
+        }
+    }
+
+}
+
+bool Item::overlapWithSnake(Snake _snake) {
+
+    for (int i = 0; i < _snake.GetSnakeLength(); i++) {
+        int nx = _snake._snakePos[i].first;
+        int ny = _snake._snakePos[i].second;
+        if ((nx == itemPos.first) || (ny == itemPos.second)) return true;
+    }
+
+    return false;
+
+}
+
+pair <ll, ll> Item::getItemPos() { return itemPos; }
+
+bool Item::isGrowthItem() { return !itemTypeCode; }
---
+++ Item.h
@@ -0,0 +1,23 @@
+#include "Map.h"
+#include "Snake.h"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Item {
+
+    public:
+        Item() {};
+        Item(Map _map, Snake _snake);
+        
+        int itemTypeCode;  // 0 : Growth // 1 : Poison
+        pair <int, int> itemPos;
+        pair <int, int> getItemPos();
+
+        void setItemPos(Map _map);
+        bool overlapWithSnake(Snake _snake);
+        bool isGrowthItem();
+
+        //bool operator==(Item& i);
+
+}
